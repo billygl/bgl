69e3923b8e89ef6c9c2acbf26a112f958e8337fc
billygl <billy.grados@gmail.com>
2022-05-25 02:35:08 -0500

    feat: add skills chart

--- components/BGSkills.vue
+++ components/BGSkills.vue
@@ -14,7 +14,13 @@
         <p class="mt-3 text-gray-600">
           
         </p>
-        
+        <div class="w-full">
+          <Bar
+            :chart-options="chartOptions"
+            :chart-data="chartData"
+            :styles="myStyles"
+          />
+        </div>
       </div>
       <BGMenu/>
       <BGFooter/>
@@ -23,19 +29,182 @@
 </template>
 
 <script>
+import _ from "lodash";
+import { Bar } from 'vue-chartjs/legacy'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ChartDataLabels)
+
+const SKILLS = [
+  {
+    "category": 1,
+    "yearsC": 11,
+    "yearsS": 9,
+    "skill": "JavaScript"
+  },
+  {
+    "category": 1,
+    "yearsC": 7,
+    "yearsS": 4,
+    "skill": "PHP"
+  },
+  {
+    "category": 2,
+    "yearsC": 4,
+    "yearsS": 3,
+    "skill": "VueJS"
+  },
+  {
+    "category": 2,
+    "yearsC": 1,
+    "yearsS": 0,
+    "skill": "ReactJS"
+  },
+  {
+    "category": 2,
+    "yearsC": 3,
+    "yearsS": 2,
+    "skill": "React Native"
+  },
+  {
+    "category": 1,
+    "yearsC": 4,
+    "yearsS": 3,
+    "skill": "Python"
+  },
+  {
+    "category": 2,
+    "yearsC": 4,
+    "yearsS": 3,
+    "skill": "Node.js"
+  },
+  {
+    "category": 1,
+    "yearsC": 6,
+    "yearsS": 5,
+    "skill": "Java"
+  },
+  {
+    "category": 2,
+    "yearsC": 6,
+    "yearsS": 5,
+    "skill": "Android"
+  },
+  {
+    "category": 2,
+    "yearsC": 10,
+    "yearsS": 9,
+    "skill": "Git"
+  },
+  {
+    "category": 3,
+    "yearsC": 3,
+    "yearsS": 3,
+    "skill": "MS-SQL"
+  },
+  {
+    "category": 3,
+    "yearsC": 6,
+    "yearsS": 4,
+    "skill": "MySQL"
+  },
+  {
+    "category": 3,
+    "yearsC": 1,
+    "yearsS": 1,
+    "skill": "MongoDB"
+  },
+  {
+    "category": 3,
+    "yearsC": 2,
+    "yearsS": 2,
+    "skill": "PostgreSQL"
+  },
+  {
+    "category": 3,
+    "yearsC": 1,
+    "yearsS": 0,
+    "skill": "Oracle Database"
+  },
+  {
+    "category": 2,
+    "yearsC": 4,
+    "yearsS": 3,
+    "skill": "Laravel"
+  },
+  {
+    "category": 2,
+    "yearsC": 1,
+    "yearsS": 1,
+    "skill": "Django"
+  }
+]
 
 export default {
-    name: "BGPortfolio",
+    name: "BGSkills",
+    components: { Bar },
     data() {
+      const skills = _.chain(SKILLS)
+      .sortBy( s => {
+        return -s.yearsS
+      })
+      .map( s => {
+        s.years = s.yearsS < 1 ? 1 : s.yearsS
+        return s
+      })
+      .value()
+      const labels = _.map(skills, 'skill')
+      const data = _.map(skills, 'years')
+      return {
+        chartData: {
+          labels,
+          datasets: [ { 
+            label: 'years',
+            data,
+            borderWidth: 2,
+            borderRadius: 20,
+          } ],
+        },
+        chartOptions: {
+          indexAxis: 'y',
+          elements: {
+            bar: {
+              borderWidth: 2,
+            }
+          },
+          xAxes: [{
+              stacked: true,
+              position: 'top'
+            },
+            {
+              type: 'category',
+              offset: true,
+              labels: ['database', 'pl', 'f']
+            }
+          ],
+          plugins: {
+            legend: {
+              position: 'bottom',
+            },
+            datalabels: {
+              anchor: 'end',
+              align: 'left'
+            }
+          },
+          responsive: true,
+          maintainAspectRatio: false,
+        }
+      }
+    },
+    computed: {
+      myStyles () {
         return {
-            
-        };
-    },
-    mounted() {
-
-    },
-    methods: {
-        
+          // width: '50vw',
+          height: '70vh',
+          position: 'relative'
+        }
+      }
     }
 }
 </script>
